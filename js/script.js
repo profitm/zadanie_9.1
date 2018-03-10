c49a260426d4bb273f3c2bc7f7c31c0455a95275
Michał Sawicki <sawicki_michal@wp.pl>
2018-03-10 18:47:16 +0100

    add return function

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var a = 2;
+var h = 5;
+var value = a * h / 2;
+
+
+function getTriangleArea(a, h) {}
+
+
+if ( (a > 0) && (h > 0) ) {
+    console.log('Parametry mają wartości większe niż 0')
+} else if ( (a <= 0) && (h <= 0) ) {
+    console.log('Nieprawidłowe dane')
+}
+
+
+return value;
+
+
+
+if (userName == 'Piotr') {
+    document.write('Witaj Piotrze!');
+} else if (userName.length == 0) {
+    document.write('Nie wprowadziłeś imienia!');
+} else {
+    document.write('Witaj ' + userName);
+}
