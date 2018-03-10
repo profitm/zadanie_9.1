ad5d5428e7b0b95b5bfbeaf90457a64ff7bf0f16
Michał Sawicki <sawicki_michal@wp.pl>
2018-03-10 18:48:55 +0100

    update js functions

--- js/script.js
+++ js/script.js
@@ -3,24 +3,18 @@ var h = 5;
 var value = a * h / 2;
 
 
-function getTriangleArea(a, h) {}
-
-
-if ( (a > 0) && (h > 0) ) {
+function getTriangleArea(a, h) {if ( (a > 0) && (h > 0) ) {
     console.log('Parametry mają wartości większe niż 0')
 } else if ( (a <= 0) && (h <= 0) ) {
     console.log('Nieprawidłowe dane')
 }
-
-
 return value;
 
-
-
-if (userName == 'Piotr') {
-    document.write('Witaj Piotrze!');
-} else if (userName.length == 0) {
-    document.write('Nie wprowadziłeś imienia!');
-} else {
-    document.write('Witaj ' + userName);
 }
+
+
+
+
+
+
+
