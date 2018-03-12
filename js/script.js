4b443550e43efb12ec0bb94e5550c6f983b14909
Michał Sawicki <sawicki_michal@wp.pl>
2018-03-12 21:11:52 +0100

    update getTriangleArea

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-var triangle1Area = getTriangleArea(10, 15)
-var triangle2Area = getTriangleArea(11, 8)
-var triangle3Area = getTriangleArea(5, 12)
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(11, 8);
+var triangle3Area = getTriangleArea(5, 12);
 
 function getTriangleArea(a, h) {
     if (a > 0 && h > 0) {
-    console.log('Parametry mają wartości większe niż 0');
-    return a * h / 2
-} else if (a <= 0 && h <= 0) {
-    console.log('Nieprawidłowe dane');
+        return a * h / 2;
+    
+} else if(a <= 0 && h <= 0) {
+    return 0;
 }
-return 0;
+
 
 }
-console.log(getTriangleArea(10, 6));
+console.log(getTriangleArea(10, 15));
 console.log(getTriangleArea(11, 8));
 console.log(getTriangleArea(5, 12));
 
