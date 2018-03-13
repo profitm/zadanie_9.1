cb24587e4e4d72834a8bd5b95d51fb3191405b40
Michał Sawicki <sawicki_michal@wp.pl>
2018-03-13 18:52:04 +0100

    beautify code

--- js/script.js
+++ js/script.js
@@ -3,22 +3,12 @@ var triangle2Area = getTriangleArea(11, 8);
 var triangle3Area = getTriangleArea(5, 12);
 
 function getTriangleArea(a, h) {
-    if (a > 0 && h > 0) {
-        return a * h / 2;
-    
-} else (a <= 0 || h <= 0) {
-    return('Nieprawidłowe dane');
-}
-
-
+	if (a > 0 && h > 0) {
+		return a * h / 2;
+	} else {
+		return 'Nieprawidłowe dane';
+	}
 }
 console.log(getTriangleArea(10, 15));
 console.log(getTriangleArea(11, 8));
-console.log(getTriangleArea(5, 12));
-
-
-
-
-
-
-
+console.log(getTriangleArea(-5, 12));
